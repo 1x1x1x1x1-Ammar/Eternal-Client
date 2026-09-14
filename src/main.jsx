6b0cdd02a500aecd0bd740cd7b5be4e0ab1a334a
ammar ahmed <boykamel8989@gmail.com>
2026-09-14 14:13:57 +0200

    ui(v1): load final motion detail layer after stable skin

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import './beta8.css';
 import './beta8-polish.css';
 import './premium.css';
 import './v1.css';
+import './v1-motion.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><HashRouter><App /></HashRouter></React.StrictMode>
