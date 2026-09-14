e2aa5b744c7e4e4dbb99ab207ff4ed918de34dd0
ammar ahmed <boykamel8989@gmail.com>
2026-09-14 17:58:48 +0200

    chore(release): report Eternal Core 1.0.1

--- eternal-core/src/main/java/gg/eternal/core/EternalCore.java
+++ eternal-core/src/main/java/gg/eternal/core/EternalCore.java
@@ -16,7 +16,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Supplier;
 
 public final class EternalCore implements ClientModInitializer {
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     private static long sessionStarted;
     private static boolean zoomed;
     private static int previousFov = 70;
