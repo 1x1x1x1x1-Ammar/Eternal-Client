2edd85bd2b33867286028756b3e9fb5ba2750df9
ammar ahmed <boykamel8989@gmail.com>
2026-09-14 12:26:39 +0200

    ui: load Beta 8 visual layer after legacy themes

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
+import './v05.css';
 import './release.css';
 import './beta6.css';
 import './beta6-core.css';
 import './beta7-core.css';
+import './beta8.css';
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter><App /></BrowserRouter>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode><HashRouter><App /></HashRouter></React.StrictMode>
 );
