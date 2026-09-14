965b26be159abd75c266920a41d4e45bc1501dae
ammar ahmed <boykamel8989@gmail.com>
2026-09-14 13:29:09 +0200

    ui: load v1 stable visual layer last

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import './beta7-core.css';
 import './beta8.css';
 import './beta8-polish.css';
 import './premium.css';
+import './v1.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><HashRouter><App /></HashRouter></React.StrictMode>
