719e5938e4042141af3519acc9e80bde0350100e
ammar ahmed <boykamel8989@gmail.com>
2026-09-17 11:12:33 +0200

    feat(ui): load v1.1 Studio presentation layer last

--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,7 @@ import './v1-motion.css';
 import './v1-systems.css';
 import './v1-transfers.css';
 import './v101-client.css';
+import './v11-studio.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><HashRouter><App /></HashRouter></React.StrictMode>
